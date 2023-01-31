a1b7191ff98d326439258c723a20627f2928f2e4
Ryuzaki01 <wagmiarmy.dev@gmail.com>
2023-01-31 21:06:56 +0700

    fix: order kind and conduit

--- src/migrations/1675173294616_fix-order.sql
+++ src/migrations/1675173294616_fix-order.sql
@@ -1,3 +1,3 @@
 -- Up Migration
-UPDATE orders SET kind = 'nftearth' WHERE source_id='0x06f0a8f8ecbbfa9599e29a52c0e191aed31cb2fe';
+UPDATE orders SET kind = 'nftearth', conduit='\xFA29f9A402157672C2F608d193526A00C6B429Af', contract='\x0f9b80fc3c8b9123d0aef43df58ebdbc034a8901' WHERE source_id='0x06f0a8f8ecbbfa9599e29a52c0e191aed31cb2fe';
 -- Down Migration
