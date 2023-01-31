bc305343863cde44b7308885ae8f61479da5c8d4
Ryuzaki01 <wagmiarmy.dev@gmail.com>
2023-01-31 19:32:06 +0700

    fix: sync tool

--- sync.js
+++ sync.js
@@ -4,15 +4,15 @@ const fetch = require('axios')
 
 console.log(`Syncing from block ${process.argv[0]} to ${process.argv[1]}`)
 
-fetch(`http://localhost:${process.env.PORT}/admin/sync-events`, {
+fetch({
+  url: `http://localhost:${process.env.PORT}/admin/sync-events`,
   method: 'POST',
-  body: JSON.stringify({
+  data: {
     "fromBlock": process.argv[0],
     "toBlock": process.argv[0]
-  }),
+  },
   headers: {
     'x-admin-api-key': process.env.ADMIN_API_KEY
   }
-}).then(res => res.json())
-  .then(console.log)
+}).then(console.log)
 .catch(console.error);
