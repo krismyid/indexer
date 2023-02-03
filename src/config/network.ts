372eaddd6690e24efc7cc9ec3462d14fe024ab3e
Ryuzaki01 <wagmiarmy.dev@gmail.com>
2023-02-03 15:26:21 +0700

    fix: paper currency

--- src/config/network.ts
+++ src/config/network.ts
@@ -264,17 +264,6 @@ export const getNetworkSettings = (): NetworkSettings => {
           // Uniswap V3: Positions NFT
           "0xc36442b4a4522e871399cd717abdd847ab11fe88",
         ],
-        whitelistedCurrencies: new Map([
-          [
-            "0x7ae1d57b58fa6411f32948314badd83583ee0e8c",
-            {
-              contract: "0x7ae1d57b58fa6411f32948314badd83583ee0e8c",
-              name: "PAPER",
-              symbol: "$PAPER",
-              decimals: 18,
-            },
-          ],
-        ]),
         supportedBidCurrencies: {
           ...defaultNetworkSettings.supportedBidCurrencies,
           // OP
