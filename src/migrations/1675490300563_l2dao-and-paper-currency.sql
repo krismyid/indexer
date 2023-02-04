219565f80ec4062018c69a82842ae6b8dd56a78f
Ryuzaki01 <wagmiarmy.dev@gmail.com>
2023-02-04 15:09:03 +0700

    fix: currency data

--- src/migrations/1675490300563_l2dao-and-paper-currency.sql
+++ src/migrations/1675490300563_l2dao-and-paper-currency.sql
@@ -24,7 +24,7 @@ INSERT INTO currencies (
                   'PAPER',
                   18,
                   '{"coingeckoCurrencyId": null, "dexScreenerId": "0x00f932f0fe257456b32deda4758922e56a4f4b42", "image": "https://nftearth.exchange/icons/currency/0x00f932f0fe257456b32deda4758922e56a4f4b42.png"}'
-                ) ON CONFLICT (contract) DO UPDATE SET name = EXCLUDED.name, metadata = EXCLUDED.metadata;
+                ) ON CONFLICT (contract) DO UPDATE SET name = EXCLUDED.name, symbol = EXCLUDED.symbol, decimals = EXCLUDED.decimals, metadata = EXCLUDED.metadata;
 -- Down Migration
 DELETE FROM currencies WHERE contract = '\xd52f94df742a6f4b4c8b033369fe13a41782bf44';
 DELETE FROM currencies WHERE contract = '\x00f932f0fe257456b32deda4758922e56a4f4b42';
