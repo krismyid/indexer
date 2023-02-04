d00c44eeedc2e72735a8463c91a644b3268f8b40
Ryuzaki01 <wagmiarmy.dev@gmail.com>
2023-02-04 14:28:16 +0700

    fix: sql query

--- src/migrations/1675490300563_l2dao-and-paper-currency.sql
+++ src/migrations/1675490300563_l2dao-and-paper-currency.sql
@@ -11,7 +11,7 @@ INSERT INTO currencies (
                   'L2DAO',
                   18,
                   '{"coingeckoCurrencyId": "optimistic-ethereum", "image": "https://assets.coingecko.com/coins/images/23699/thumb/Khp7Y4Sn.png?1645081048"}'
-                ) ON CONFLICT DO UPDATE SET name = EXCLUDED.name;
+                ) ON CONFLICT (contract) DO UPDATE SET name = EXCLUDED.name;
 INSERT INTO currencies (
                   contract,
                   name,
@@ -24,7 +24,7 @@ INSERT INTO currencies (
                   'PAPER',
                   18,
                   '{"coingeckoCurrencyId": null, "dexScreenerId": "0x00f932f0fe257456b32deda4758922e56a4f4b42", "image": "https://nftearth.exchange/icons/currency/0x00f932f0fe257456b32deda4758922e56a4f4b42.png"}'
-                ) ON CONFLICT DO UPDATE SET name = EXCLUDED.name, metadata = EXCLUDED.metadata;
+                ) ON CONFLICT (contract) DO UPDATE SET name = EXCLUDED.name, metadata = EXCLUDED.metadata;
 -- Down Migration
 DELETE FROM currencies WHERE contract = '\xd52f94df742a6f4b4c8b033369fe13a41782bf44';
 DELETE FROM currencies WHERE contract = '\x00f932f0fe257456b32deda4758922e56a4f4b42';
